8290f7cf770ad4e47f983e7d03bcdb8ba66dcebd
Moody Salem <moody.salem@gmail.com>
2019-07-13 13:36:54 -0500

    Update dependencies and clean up provider a bit

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,17 @@ import { IFrameEthereumProvider } from '@ethvault/iframe-provider';
 
 /**
  * Return true if the current window context appears to be embedded within an iframe element.
- *
- * This should be checked before the provider is used.
  */
 export function isEmbeddedInIFrame(): boolean {
-  return window && window.parent && window.parent !== window.self;
+  return window && window.parent && window.self && window.parent !== window.self;
 }
 
-if (typeof window !== 'undefined') {
-  if (isEmbeddedInIFrame()) {
-    const target = window as any;
+if (isEmbeddedInIFrame()) {
+  const target = window as any;
 
-    const provider = new IFrameEthereumProvider();
+  const provider = new IFrameEthereumProvider();
 
-    target.web3 = provider;
-    target.web3.currentProvider = provider;
-    target.ethereum = provider;
-  }
+  target.web3 = provider;
+  target.web3.currentProvider = provider;
+  target.ethereum = provider;
 }
